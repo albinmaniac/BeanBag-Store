956b41ade11f793bba3af48f4bde7dfc281423c8
albinmaniac <www.albinmania@gmail.com>
2025-04-24 14:08:52 +0530

    added

--- bean_store/settings.py
+++ bean_store/settings.py
@@ -124,13 +124,9 @@ AUTHENTICATION_BACKENDS = [
 # Provider specific settings
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
-        # For each OAuth based provider, either add a ``SocialApp``
-        # (``socialaccount`` app) containing the required client
-        # credentials, or list them here:
         'APP': {
-            'client_id': config('GOOGLE_CLIENT_ID'),
-            'secret': config('GOOGLE_SECRET'),
-            
+            'client_id': config('GOOGLE_CLIENT_ID', default=''),
+            'secret':    config('GOOGLE_SECRET',    default=''),
         }
     }
 }
